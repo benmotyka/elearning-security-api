92d7739dce108f63616e777fb6c627b5e8da7523
ben <rikenpl99@gmail.com>
2021-07-03 22:10:44 +0200

    changes

--- seeders/quizQuestions/quizQuestions.data.js
+++ seeders/quizQuestions/quizQuestions.data.js
@@ -15,4 +15,28 @@ export const quizQuestionsData = [
         ],
         correctAnswerIndex: 3
     },
+    {
+        courseTitle: "Phishing",
+        question: "Question dla phishing",
+        answers: [
+            "Odpowiedz 5", "Odpowiedz 6", " Odpowiedz 7", "Odpowiedz 8"
+        ],
+        correctAnswerIndex: 3
+    },
+    {
+        courseTitle: "SQL Injection",
+        question: "Question dla sql injection 3",
+        answers: [
+            "Odpowiedz 5", "Odpowiedz 6", " Odpowiedz 7", "Odpowiedz 8"
+        ],
+        correctAnswerIndex: 3
+    },
+    {
+        courseTitle: "SQL Injection",
+        question: "Question dla sql injection 4",
+        answers: [
+            "Odpowiedz 5", "Odpowiedz 6", " Odpowiedz 7", "Odpowiedz 8"
+        ],
+        correctAnswerIndex: 3
+    },
 ]
