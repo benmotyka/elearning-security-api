1e9d7cbbd4c6a937e0fc6d2965e394e5f146c0e2
ben <rikenpl99@gmail.com>
2021-07-08 22:17:59 +0200

    corst test

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors())
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://staysecure.pl");
   res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") {
