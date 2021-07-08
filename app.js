f500ff589c8d874dcd08811adb72e4d91184bd26
ben <rikenpl99@gmail.com>
2021-07-08 23:51:45 +0200

    cors fixing

--- app.js
+++ app.js
@@ -16,9 +16,8 @@ dotenv.config();
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://staysecure.pl");
-  res.header("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
