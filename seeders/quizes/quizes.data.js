00abcc49bcbc08369410e1e898ae7363b0f6bd90
Ben <rikenpl99@gmail.com>
2021-12-27 23:51:42 +0100

    added quiz legit questions and answers for sql inj and ransomware

--- seeders/quizes/quizes.data.js
+++ seeders/quizes/quizes.data.js
@@ -3,66 +3,84 @@ export const quizesData = [
     courseLink: "ransomware",
     items: [
       {
-        question: "Pytanie testowe 1",
+        question: "Co jest głównym celem ransomware?",
         answers: [
           {
-            text: "Odp 1",
+            text: "Wykradnięcie danych od użytkownika",
             isCorrect: false,
           },
           {
-            text: "Odp 2",
+            text: "Usunięcie wszystkich danych na komputerze ofiary",
+            isCorrect: false,
+          },
+          {
+            text: "Zaszyfrowanie danych na urządzeniu końcowym",
             isCorrect: true,
           },
           {
-            text: "Odp 3",
-            isCorrect: false,
-          },
-          {
-            text: "Odp 4",
+            text: "Infekcja przeglądarki celem wyświetlania reklam",
             isCorrect: false,
           },
         ],
       },
       {
-        question: "Pytanie testowe 2",
+        question: "Częste wykonywanie kopii zapasowej danych nie chroni całkowicie przed konsekwencjami ransomware",
         answers: [
           {
-            text: "Odp 1",
-            isCorrect: false,
-          },
-          {
-            text: "Odp 2",
-            isCorrect: false,
-          },
-          {
-            text: "Odp 3",
+            text: "Prawda",
             isCorrect: true,
           },
           {
-            text: "Odp 4",
+            text: "Fałsz",
             isCorrect: false,
+          }
+        ],
+      },
+      {
+        question: "Którym z wymienionych sposobów, ransomware zazwyczaj NIE DOSTAJE SIĘ do urządzenia końcowego?",
+        answers: [
+          {
+            text: "Zdalne wykonanie kodu",
+            isCorrect: false,
+          },
+          {
+            text: "Maile phishingowe",
+            isCorrect: false,
+          },
+          {
+            text: "Zdalny dostęp",
+            isCorrect: false,
+          },
+          {
+            text: "Sieć Wi-Fi",
+            isCorrect: true,
           },
         ],
       },
       {
-        question: "Pytanie testowe 3",
+        question: "W przypadku infekcji ransomware, pierwszą rzeczą, którą należy zrobić, jest zapłata okupu",
         answers: [
           {
-            text: "Odp 1",
+            text: "Prawda",
             isCorrect: false,
           },
           {
-            text: "Odp 2",
-            isCorrect: false,
-          },
-          {
-            text: "Odp 3",
+            text: "Fałsz",
             isCorrect: true,
-          },
+          }
+        ],
+      },
+      {
+        question: "Używanie dobrego programu antywirusowego całkowicie eliminuje zagrożenie ransomware",
+        answers: [
           {
-            text: "Odp 4",
+            text: "Prawda",
             isCorrect: false,
           },
+          {
+            text: "Fałsz",
+            isCorrect: true,
+          }
         ],
       },
     ],
@@ -71,64 +89,98 @@ export const quizesData = [
     courseLink: "sql-injection",
     items: [
       {
-        question: "Pytanie testowe 1",
+        question: "Na czym polega atak SQL Injection?",
         answers: [
           {
-            text: "Odp 1",
+            text: "Na uruchomieniu na urządzeniu końcowym ofiary programu, który zaszyfruje wszystkie jej dane",
             isCorrect: false,
           },
           {
-            text: "Odp 2",
+            text: "Na przekierowaniu użytkowników sieci Wi-Fi do spreparowanej strony internetowej",
+            isCorrect: false,
+          },
+          {
+            text: "Na całkowitym odebraniu dostępu do strony internetowej ofiary",
+            isCorrect: false,
+          },
+          {
+            text: "Na znalezieniu luki w oprogramowaniu i wykorzystaniu jej",
+            isCorrect: true,
+          },
+        ],
+      },
+      {
+        question: "Atak SQL Injection nie ma związku z bazami danych",
+        answers: [
+          {
+            text: "Fałsz",
             isCorrect: true,
           },
           {
-            text: "Odp 3",
+            text: "Prawda",
+            isCorrect: false,
+          }
+        ],
+      },
+      {
+        question: "Jakie narzędzie znacznie ułatwia atakującemu znalezienie podatności?",
+        answers: [
+          {
+            text: "Serwer proxy",
+            isCorrect: true,
+          },
+          {
+            text: "Keylogger",
             isCorrect: false,
           },
           {
-            text: "Odp 4",
+            text: "Skanowanie portów",
+            isCorrect: false,
+          },
+          {
+            text: "Blokada usług",
             isCorrect: false,
           },
         ],
       },
       {
-        question: "Pytanie testowe 2",
+        question: "Jakie polecenie języka SQL wykonuje zapytanie zwracające dane z bazy?",
         answers: [
           {
-            text: "Odp 1",
+            text: "ALTER DATABASE",
             isCorrect: false,
           },
           {
-            text: "Odp 2",
+            text: "UPDATE",
             isCorrect: false,
           },
           {
-            text: "Odp 3",
+            text: "SELECT",
             isCorrect: true,
           },
           {
-            text: "Odp 4",
+            text: "ALTER TABLE",
             isCorrect: false,
           },
         ],
       },
       {
-        question: "Pytanie testowe 3",
+        question: "Jakie są najlepsze praktyki na ochronę przed atakami SQL Injection",
         answers: [
           {
-            text: "Odp 1",
+            text: "Instalacja antywirusa i monitorowanie aktywności sieciowej na urządzeniu",
             isCorrect: false,
           },
           {
-            text: "Odp 2",
-            isCorrect: false,
-          },
-          {
-            text: "Odp 3",
+            text: "Dokładne filtrowanie i czyszczenie danych przesyłanych do bazy danych od użytkownika",
             isCorrect: true,
           },
           {
-            text: "Odp 4",
+            text: "Uniemożliwienie w formularzu interfejsu użytkownika przesłania znaków specjalnych",
+            isCorrect: false,
+          },
+          {
+            text: "Stosowanie zasady ograniczonego zaufania",
             isCorrect: false,
           },
         ],
